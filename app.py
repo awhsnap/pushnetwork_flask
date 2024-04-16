d127ef91b43d8baa7fbb204b82194afd0a6622b3
Adrian <awhsnap@yahoo.com>
2024-04-16 03:29:51 -0500

    templates/base.html for jinja, add route

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 helloworld = Flask(__name__)
 @helloworld.route('/')
 def run():
   return "{\"message\":\"Python\"}"
 
+@helloworld.route('/home')
+def home():
+  return render_template('base.html')
 if __name__ == "__main__":
   helloworld.run(host="0.0.0.0", port=int("4321"), debug=True)
