17bfbf8011e67ed5a6bcf87ec330ee93ee1c80f3
Adrian <awhsnap@yahoo.com>
2024-04-16 04:34:12 -0500

    no test, commit

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ def run():
 def home():
   # get existing messages 
   existing_messages = []
+  
+  return render_template('base.html', data=existing_messages)
+
+@helloworld.route('/create_message')
+def create_message():
   # create a message from input
   create_message = ""
   # store message into sqlite
@@ -24,8 +29,6 @@ def home():
   c.execute("INSERT INTO messages VALUES (?)", (create_message,))
   conn.commit()
   conn.close()
-
-  return render_template('base.html', data=existing_messages)
 
 if __name__ == "__main__":
   helloworld.run(host="0.0.0.0", port=int("5000"), debug=True)
